395af1eb40e04c2f82b197dede5bbfab5e93fc03
synq <nothingdudeasdasd@gmail.com>
2024-06-17 22:43:56 -0700

    kitlibr

---
+++ kitlibr.lua
@@ -0,0 +1,29 @@
+wait(2)
+
+print("init kitlibr")
+
+local function attach()
+    if readfile("kitlibr/attached.klr") == "[ kitlibr 1.0.0 ]" then
+        writefile("kitlibr/attached.klr", "read")
+    end
+end
+
+local function exec()
+    if isfile("kitlibr/execution.klr") then
+        local output = readfile("kitlibr/execution.klr")
+        if output then
+            xpcall(function()
+                loadstring(output)()
+            end, function(err)
+                warn(err)
+            end)
+        end
+        delfile("kitlibr/execution.klr")  -- Delete the file after execution
+    end
+end
+
+while true do
+    attach()
+    exec()
+    wait(0.1)
+end
